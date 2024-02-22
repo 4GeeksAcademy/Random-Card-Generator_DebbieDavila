8737866dae7bdcdd29c6192734e86f6a04e30ce8
Debbie Dávila <146867711+DebbieDavilaDev@users.noreply.github.com>
2024-02-22 20:39:19 +0000

    random card generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,50 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const suits = ['<font color="red">♥</font>', "♦", "♠", "♣"];
+const values = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "Jack",
+  "Queen",
+  "King",
+  "Ace"
+];
+
+function generateRandomCard() {
+  // Randomly select a suit and a value
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+  // Construct and return the random card
+  document.getElementById("top").innerHTML = randomSuit;
+  document.getElementById("bottom").innerHTML = randomSuit;
+  document.getElementById("middle").innerHTML = randomValue;
+  return `${randomValue} of ${randomSuit}`;
+}
+
+// Execute the function when the page loads
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const randomCard = generateRandomCard();
+  console.log("Random Card:", randomCard);
 };
+
+function handleWindowLoad() {
+  const randomCard = generateRandomCard();
+  console.log("Random Card:", randomCard);
+}
+
+window.addEventListener("load", handleWindowLoad);
+
+let button = document.getElementById("generateButton");
+button.addEventListener("click", function() {
+  // Your code here
+
+  let randomCard = generateRandomCard();
+});
